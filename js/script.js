1fa12b6985880227935f0ea53d480fd135a91efd
Caroline Yanko <karolina.yanny@gmail.com>
2014-09-26 12:45:32 +0300

    react testing

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-(function(){
-	var congrats = document.getElementsByClassName('congrats')[0],
-			counter = document.getElementsByClassName('counter')[0],
-			clickCounter = 0;
-
-			congrats.addEventListener('click', function(){
-				clickCounter ++;
-				counter.innerText = 'You was congrats ' + clickCounter + ' times';
-			}, false);
-})();
+/** @jsx React.DOM */
+React.renderComponent(
+  <h1>Hello, world!</h1>,
+  document.getElementById('container')
+);
+/*(function(){
+})();*/
